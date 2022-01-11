f6cf12b23db37cd7d5ae6f888893fe4e2371b70f
acsmit44 <acsmit44@asu.edu>
2022-01-10 23:51:41 -0700

    Added filtering options for the table

--- src/pages/table-manager.js
+++ src/pages/table-manager.js
@@ -9,15 +9,89 @@ class TableManager {
             "jlpt",
             "wanikani"
         ];
+        this._tableSearchFields = {
+            japanese: {
+                index: 0,
+                value: '',
+                filterFunc: (enteredVal, cellVal) => {
+                    return cellVal.indexOf(enteredVal) > -1;
+                }
+            },
+            english: {
+                index: 1,
+                value: '',
+                filterFunc: (enteredVal, cellVal) => {
+                    const enteredLen = enteredVal.length;
+                    const words = cellVal.match(/[a-z]+/gi);
+                    for (const word of words) {
+                        if (word.slice(0, enteredLen).indexOf(enteredVal) > -1) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            },
+            partsOfSpeech: {
+                index: 2,
+                value: '',
+                filterFunc: (enteredVal, cellVal) => {
+                    const enteredLen = enteredVal.length;
+                    const words = cellVal.match(/[a-z]+/gi);
+                    for (const word of words) {
+                        if (word.slice(0, enteredLen).indexOf(enteredVal) > -1) {
+                            return true;
+                        }
+                    }
+                    return false;
+                }
+            },
+            common: {
+                index: 3,
+                value: '',
+                filterFunc: (enteredVal, cellVal) => {
+                    return cellVal !== "N/A";
+                }
+            },
+            jlpt: {
+                index: 4,
+                value: '',
+                filterFunc: (enteredVal, cellVal) => {
+                    return cellVal.indexOf(enteredVal) > -1;
+                }
+            },
+            wanikani: {
+                index: 5,
+                value: '',
+                filterFunc: (enteredVal, cellVal) => {
+                    return cellVal.indexOf(enteredVal) > -1;
+                }
+            },
+            added: {
+                index: 6,
+                value: '',
+                filterFunc: (enteredVal, cellVal) => {
+                    return cellVal === "add";
+                }
+            }
+        };
     }
 
     initTable(wordObjs) {
-        this._table.style.display = "none";
+        const table = this._table.closest("table");
+        table.style.display = "none";
         while (this._table.firstChild) {
             this._table.removeChild(this._table.firstChild);
         }
         this.insertRows(wordObjs);
-        this._table.removeAttribute("style");
+        table.style.display = "";
+        table.style["table-layout"] = "fixed";
+        table.style["word-wrap"] = "break-word";
+        const searchFilters = document.querySelector("thead > tr");
+        for (const searchFilter of searchFilters.querySelectorAll("input")) {
+            searchFilter.oninput = event => {
+                this._filterTable();
+            };
+        }
     }
 
     insertRows(wordObjs) {
@@ -36,27 +110,64 @@ class TableManager {
             newCell.appendChild(newText);
         }
         newCell = newRow.insertCell();
-        const newAddBtn = newAddButtonElement(wordObj.noteID !== -1);
+        const newAddBtn = this._newAddButtonElement(wordObj.noteID !== -1);
         newCell.appendChild(newAddBtn);
     }
-}
 
-function newAddButtonElement(hasNoteID) {
-    const buttonWrapper = document.createElement("div");
-    buttonWrapper.className += "button-wrapper";
-    const addButton = document.createElement("button");
-    addButton.className += "button-pretty";
-    addButton.disabled = hasNoteID;
-    const addButtonText = document.createElement("span");
-    addButtonText.textContent = hasNoteID ? "Added" : "Add";
-    addButton.appendChild(addButtonText);
-    buttonWrapper.appendChild(addButton);
-    if (!hasNoteID) {
-        addButton.addEventListener("click", event => {
-            addNoteOnClick(event);
-        });
-    } else {
-        turnOffButton(addButton);
+    _updateSearchValues() {
+        for (const filterType of Object.keys(this._tableSearchFields)) {
+            const input = document.querySelector(`#filter-${filterType}`);
+            if (input.type === "text") {
+                this._tableSearchFields[filterType].value = input.value.toLowerCase();
+            } else if (input.type === "checkbox") {
+                this._tableSearchFields[filterType].value = input.checked ? "y" : "";
+            }
+        }
     }
-    return buttonWrapper;
+
+    _filterTable() {
+        this._updateSearchValues();
+        const rows = document.querySelectorAll("tbody tr");
+        for (const row of rows) {
+            let show = true;
+            for (const [key, searchFilter] of Object.entries(this._tableSearchFields)) {
+                if (searchFilter.value !== '') {
+                    const cellVal = this._getRowColumnValue(row, searchFilter.index);
+                    show &= searchFilter.filterFunc(searchFilter.value, cellVal.toLowerCase());
+                    if (!show) { break; }
+                } else {
+                    show &= true;
+                }
+            }
+            this._hideOrUnhideRow(row, show);
+        }
+    }
+
+    _hideOrUnhideRow(row, shouldShow) {
+        row.style.display = shouldShow ? "" : "none";
+    }
+
+    _getRowColumnValue(row, colIndex) {
+        return row.cells[colIndex].textContent || row.cells[colIndex].innerText;
+    }
+
+    _newAddButtonElement(hasNoteID) {
+        const buttonWrapper = document.createElement("div");
+        buttonWrapper.className += "button-wrapper";
+        const addButton = document.createElement("button");
+        addButton.className += "button-pretty";
+        addButton.disabled = hasNoteID;
+        const addButtonText = document.createElement("span");
+        addButtonText.textContent = hasNoteID ? "Added" : "Add";
+        addButton.appendChild(addButtonText);
+        buttonWrapper.appendChild(addButton);
+        if (!hasNoteID) {
+            addButton.addEventListener("click", event => {
+                addNoteOnClick(event);
+            });
+        } else {
+            turnOffButton(addButton);
+        }
+        return buttonWrapper;
+    }
 }
